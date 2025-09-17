eacb7b214a8a4c543901e6f25c996ed8a86c86a6
Tomas Gustavsson <tomas.gustavsson@keyfactor.com>
2025-09-17 13:54:43 +0200

    syn parameters with p11v3.2 (#65)

--- src/main/java/org/pkcs11/jacknji11/CKP.java
+++ src/main/java/org/pkcs11/jacknji11/CKP.java
@@ -25,10 +25,33 @@ import java.util.Map;
 
 /**
  * CKP_? constants.
- * @author Joel Hockey (joel.hockey@gmail.com)
+ * https://github.com/oasis-tcs/pkcs11/blob/master/working/headers/pkcs11t.h
  */
 public class CKP {
     public static final long CKP_PKCS5_PBKD2_HMAC_SHA1 = 0x00000001;
+
+    public static final long ML_DSA_44           = 0x00000001;
+    public static final long ML_DSA_65           = 0x00000002;
+    public static final long ML_DSA_87           = 0x00000003;
+
+    /* SLH-DSA values for CKA_PARAMETER_SETS */
+    public static final long SLH_DSA_SHA2_128S   = 0x00000001;
+    public static final long SLH_DSA_SHAKE_128S  = 0x00000002;
+    public static final long SLH_DSA_SHA2_128F   = 0x00000003;
+    public static final long SLH_DSA_SHAKE_128F  = 0x00000004;
+    public static final long SLH_DSA_SHA2_192S   = 0x00000005;
+    public static final long SLH_DSA_SHAKE_192S  = 0x00000006;
+    public static final long SLH_DSA_SHA2_192F   = 0x00000007;
+    public static final long SLH_DSA_SHAKE_192F  = 0x00000008;
+    public static final long SLH_DSA_SHA2_256S   = 0x00000009;
+    public static final long SLH_DSA_SHAKE_256S  = 0x0000000a;
+    public static final long SLH_DSA_SHA2_256F   = 0x0000000b;
+    public static final long SLH_DSA_SHAKE_256F  = 0x0000000c;
+
+    /* ML-KEM values for CKA_PARAMETER_SETS */
+    public static final long ML_KEM_512          = 0x00000001;
+    public static final long ML_KEM_768          = 0x00000002;
+    public static final long ML_KEM_1024         = 0x00000003;
 
     /** Maps from long value to String description (variable name). */
     private static final Map<Long, String> L2S = C.createL2SMap(CKP.class);
@@ -39,3 +62,4 @@ public class CKP {
      */
     public static final String L2S(long ckp) { return C.l2s(L2S, CKP.class.getSimpleName(), ckp); }
 }
+
